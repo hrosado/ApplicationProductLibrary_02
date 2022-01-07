7a920ae561fc33e3ca7a1ca7063d614cb554efda
Hommy Rosado <hommy.rosado@gmail.com>
2022-01-06 19:00:53 -0500

    Working and completed.

--- Apl_Mvc_App/Controllers/HomeController.cs
+++ Apl_Mvc_App/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Apl_Mvc_App.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,35 @@ namespace Apl_Mvc_App.Controllers
 {
     public class HomeController : Controller
     {
+        private AplDbEntities db = new AplDbEntities();
+
+        [Serializable]
+        private class FileItems
+        {
+            public int Id { get; set; }
+            public string FileName { get; set; }
+            public string FileSize { get; set; }
+            public DateTime TimeStamp { get; set; }
+
+
+        }
+
+        [HttpGet]
+        public JsonResult GetData()
+        {
+            var data = (from file in db.APLFileItems
+                          select file).ToList();
+
+                               //string jsonString = (from file in db.APLFileItems
+                               //                     select file).ToString();
+
+            //var results = JsonConvert.DeserializeObject<List<APLFileItem>>(data.ToString());
+            // return Json(new { data = results });
+            return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+        }
         public ActionResult Index()
         {
+
             return View();
         }
 
